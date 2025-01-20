2eb8df8850fbc846af831b438ee330f8f23d0cf3
zadiki ochola hassan <zadikiochola@gmail.com>
2025-01-20 10:12:41 +0300

    student model generation

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  apiService = inject(ApiService);
+  studentList: Student[] = signal([]);
+  generateStudentsForm = new FormGroup({
+    count: new FormControl(0),
+  });
   ngOnInit() {}
+
+  onClickGenerateCsv() {
+    this.apiService.get('student/generateStudentsCSV').subscribe({
+      next: (response: any) => {
+        Swal.fire({
+          title: `Csv success`,
+          text: 'you have generated csv successfully',
+          icon: 'success',
+        });
+      },
+      error: (err?) => {
+        console.error(err);
+        Swal.fire({
+          title: `CSV error`,
+          text: 'Error generating csv',
+          icon: 'error',
+        });
+      },
+    });
+  }
+  onClickSaveStudents() {
+    this.apiService.get('student/saveGeneratedStudents').subscribe({
+      next: (response: any) => {
+        console.log(response);
+      },
+      error: (err?) => {
+        console.error(err);
+        alert('error saving students');
+      },
+    });
+  }
+
+  onSubmit() {
+    const { count } = this.generateStudentsForm.value;
+    this.apiService.get('student/generateStudentsExcel', { count }).subscribe({
+      next: (response: any) => {
+        this.studentList.set(response);
+        Swal.fire({
+          title: `Students generated `,
+          text: `you have successfully generated ${count} students`,
+          icon: 'success',
+        });
+      },
+      error: (err?) => {
+        console.error(err);
+        Swal.fire({
+          title: 'generation error',
+          text: 'error generating students',
+          icon: 'error',
+        });
+      },
+    });
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,13 +4,12 @@ import { Store } from '@ngrx/store';
 
 import { logout } from '../../actions/user.action';
 
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../../services/api.service';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RouterOutlet, ReactiveFormsModule],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -19,9 +18,7 @@ export class HomeComponent {
   router = inject(Router);
   store = inject(Store);
   apiService = inject(ApiService);
-  generateStudentsForm = new FormGroup({
-    count: new FormControl(0),
-  });
+
   ngOnInit() {
     this.store
       .select('user')
@@ -30,55 +27,5 @@ export class HomeComponent {
   onLogout() {
     this.store.dispatch(logout());
     this.router.navigate(['/login']);
-  }
-  onSubmit() {
-    const { count } = this.generateStudentsForm.value;
-    this.apiService.get('student/generateStudentsExcel', { count }).subscribe({
-      next: (response: any) => {
-        Swal.fire({
-          title: `Students generated `,
-          text: `you have successfully generated ${count} students`,
-          icon: 'success',
-        });
-      },
-      error: (err?) => {
-        console.error(err);
-        Swal.fire({
-          title: 'generation error',
-          text: 'error generating students',
-          icon: 'error',
-        });
-      },
-    });
-  }
-  onClickGenerateCsv() {
-    this.apiService.get('student/generateStudentsCSV').subscribe({
-      next: (response: any) => {
-        Swal.fire({
-          title: `Csv success`,
-          text: 'you have generated csv successfully',
-          icon: 'success',
-        });
-      },
-      error: (err?) => {
-        console.error(err);
-        Swal.fire({
-          title: `CSV error`,
-          text: 'Error generating csv',
-          icon: 'error',
-        });
-      },
-    });
-  }
-  onClickSaveStudents() {
-    this.apiService.get('student/saveGeneratedStudents').subscribe({
-      next: (response: any) => {
-        console.log(response);
-      },
-      error: (err?) => {
-        console.error(err);
-        alert('error saving students');
-      },
-    });
   }
 }
